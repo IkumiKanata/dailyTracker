7aad1e041351b3d55241360b0969c64438b769b1
KanataIkumi <i.kanata0531@gmail.com>
2024-07-25 14:07:04 +0900

    add @CrossOrigin

--- src/main/java/com/example/dailytracker/controller/HabitController.java
+++ src/main/java/com/example/dailytracker/controller/HabitController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/habit")
+@CrossOrigin
 public class HabitController {
 
     private final HabitService habitService;
