9072233894e026da6f4c134dd3c60f583ba0a066
KanataIkumi <i.kanata0531@gmail.com>
2024-07-31 22:56:21 +0900

    add role

---
+++ src/main/java/com/example/dailytracker/entity/Role.java
@@ -0,0 +1,21 @@
+package com.example.dailytracker.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+
+@Entity
+@Table(name = "role")
+@Data
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleId;
+
+    @Column
+    private String authority;
+
+
+}
